f27033bcf9ab034cc95972d47dd23708a67f03cb
Stephen <yoonic@gmail.com>
2019-05-19 14:40:13 -0400

    Added FA icons & Reach Router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import Home from './Home';
 import Welcome from './Welcome';
+import Navigation from './Navigation';
+import Login from './Login';
+import Register from './Register';
+import Meetings from './Meetings';
+import { Router } from '@reach/router';
 
 class App extends Component {
   constructor() {
@@ -13,8 +18,15 @@ class App extends Component {
   render() {
     return (
       <div>
+        <Navigation user={this.state.user} />
         {this.state.user && <Welcome user={this.state.user} />}
-        <Home user={this.state.user} />
+
+        <Router>
+          <Home path="/" user={this.state.user} />
+          <Login path="/login" />
+          <Meetings path="/meetings" />
+          <Register path="/register" />
+        </Router>
       </div>
     );
   }

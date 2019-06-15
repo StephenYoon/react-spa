29e835f6ac859e1e77cb960978458f460ffda910
Stephen <yoonic@gmail.com>
2019-06-15 10:32:54 -0400

    Added logic to add meetings to firebase

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+// Import React
 import React, { Component } from 'react';
 import { Router, navigate } from '@reach/router';
 import firebase from './Firebase';
@@ -62,6 +63,13 @@ class App extends Component {
       });
   };
 
+  addMeeting = meetingName => {
+    const ref = firebase
+      .database()
+      .ref(`meetings/${this.state.user.uid}`);
+    ref.push({ meetingName: meetingName });
+  };
+
   render() {
     return (
       <div>
@@ -79,7 +87,7 @@ class App extends Component {
         <Router>
           <Home path="/" user={this.state.user} />
           <Login path="/login" />
-          <Meetings path="/meetings" />
+          <Meetings path="/meetings" addMeeting={this.addMeeting} />
           <Register
             path="/register"
             registerUser={this.registerUser}
